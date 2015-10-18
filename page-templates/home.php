459ff2a77019f75e414f5b7e3cb9b28ce08a43f8
Damian Boni <damianboni@gmail.com>
2015-10-18 22:02:23 +0100

    template and styling changes.

--- page-templates/home.php
+++ page-templates/home.php
@@ -17,34 +17,13 @@ get_header(); ?>
     </ol>
     <div class="carousel-inner">
         <div class="item">
-            <img data-src="holder.js/900x500/auto/#777:#7a7a7a/text:First slide" alt="First slide" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI5MDAiIGhlaWdodD0iNTAwIj48cmVjdCB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iIzc3NyI+PC9yZWN0Pjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjQ1MCIgeT0iMjUwIiBzdHlsZT0iZmlsbDojN2E3YTdhO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1zaXplOjU2cHg7Zm9udC1mYW1pbHk6QXJpYWwsSGVsdmV0aWNhLHNhbnMtc2VyaWY7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+Rmlyc3Qgc2xpZGU8L3RleHQ+PC9zdmc+">
-            <div class="container">
-                <div class="carousel-caption">
-                    <h1>Metallic and Pearlescent paints require application of a protective lacquer once colour coat has dried.</h1>
-                    <p>picture for illustration only</p>
-                    <p><a class="btn btn-lg btn-primary hvr-sweep-to-right" href="#" role="button">Sign up today</a></p>
-                </div>
-            </div>
+            <img alt="First slide" src="<?php echo the_field('slide_one_image');?>">
         </div>
         <div class="item">
-            <img data-src="holder.js/900x500/auto/#666:#6a6a6a/text:Second slide" alt="Second slide" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI5MDAiIGhlaWdodD0iNTAwIj48cmVjdCB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iIzY2NiI+PC9yZWN0Pjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjQ1MCIgeT0iMjUwIiBzdHlsZT0iZmlsbDojNmE2YTZhO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1zaXplOjU2cHg7Zm9udC1mYW1pbHk6QXJpYWwsSGVsdmV0aWNhLHNhbnMtc2VyaWY7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+U2Vjb25kIHNsaWRlPC90ZXh0Pjwvc3ZnPg==">
-            <div class="container">
-                <div class="carousel-caption">
-                    <h1>Another example headline.</h1>
-                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                    <p><a class="btn btn-lg btn-primary hvr-sweep-to-right" href="#" role="button">Learn more</a></p>
-                </div>
-            </div>
+            <img alt="Second slide" src="<?php echo the_field('slide_two_image');?>">
         </div>
         <div class="item active">
-            <img data-src="holder.js/900x500/auto/#555:#5a5a5a/text:Third slide" alt="Third slide" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI5MDAiIGhlaWdodD0iNTAwIj48cmVjdCB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iIzU1NSI+PC9yZWN0Pjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjQ1MCIgeT0iMjUwIiBzdHlsZT0iZmlsbDojNWE1YTVhO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1zaXplOjU2cHg7Zm9udC1mYW1pbHk6QXJpYWwsSGVsdmV0aWNhLHNhbnMtc2VyaWY7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+VGhpcmQgc2xpZGU8L3RleHQ+PC9zdmc+">
-            <div class="container">
-                <div class="carousel-caption">
-                    <h1>One more for good measure.</h1>
-                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                    <p><a class="btn btn-lg btn-primary hvr-sweep-to-right" href="#" role="button">Browse gallery</a></p>
-                </div>
-            </div>
+            <img alt="Third slide" src="<?php echo the_field('slide_three_image');?>">
         </div>
     </div>
     <a class="left carousel-control" href="#carousel-id" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
@@ -54,57 +33,44 @@ get_header(); ?>
 
 <div class="row get-quote">
 	<div class="col-xs-12 col-sm-4 col-sm-offset-3">
-		<h2 class="text-uppercase">You can check out these other items that we have on eBay right now!</h2>
+		<h2 class="text-uppercase"><?php echo the_field('cta_one_text');?></h2>
 	</div>
 	<div class="col-xs-12 col-sm-4">
-		<button type="button" class="btn btn-default promo-button hvr-sweep-to-right">Visit our Ebay store</button>
+		<a href="<?php echo the_field('cta_one_button_url');?>" class="btn btn-default promo-button hvr-sweep-to-right"><?php echo the_field('cta_one_button');?></a>
 	</div>
 </div>
 
 <div class="row">
 	<div class="col-xs-12 col-sm-8 col-sm-offset-2 text-center">
-		<h2 class="text-uppercase">Why Choose Us?</h2>
-		<p>
-			We are a company with over twenty years of experience in manufacturing automotive paints to make independent repairs that are based on the original vehicle paints. We produce high quality touch-up, which supplement the small scratches and paint chipping. We offer more than 800 colors of acrylic varnishes touch up to brand cars such as Fiat, Opel, Volkswagen, Seat, Audi, Skoda, Daewoo, Renault, Ford, Citroen, Peugeot, Kia, Honda, Toyota, Nissan, Suzuki, Land Rover, Mercedes, BMW and constantly expanding our range.
-		</p>
-		<p>
-			Our products are based on in-depth research, knowledge and years of experience of our employees. The fact that our adjusters are metallic and pearl shine in one layer, while maintaining high color fidelity, deliver them to do dealership networks and production lines New Car Fiat and Opel.
-		</p>
+		<h2 class="text-uppercase"><?php echo the_field('content_heading');?></h2>
+		<?php echo the_field('content_text');?>
 	</div>
 </div>
 <div class="row promo-icons">
 	<div class="col-xs-12 col-sm-4 col-sm-offset-2">
-		<i class="fa fa-balance-scale"></i>
-		<h3>Vehicle Parts & Accessories</h3>
-		<p>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta excepturi vero qui, recusandae alias omnis facere laudantium sapiente velit error, eaque illum ab voluptatum, non.
-		</p>
+		<?php echo the_field('icon_one');?>
+		<h3><?php echo the_field('icon_one_heading');?></h3>
+		<?php echo the_field('icon_one_text');?>
 	</div>
 	<div class="col-xs-12 col-sm-4">
-		<i class="fa fa-chrome"></i>
-		<h3>Car Accessories</h3>
-		<p>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto eius voluptatibus dolorem officiis, vitae voluptates sed ut impedit dolor molestiae, eaque voluptate at. Tempore, expedita!
-		</p>
+		<?php echo the_field('icon_two');?>
+		<h3><?php echo the_field('icon_two_heading');?></h3>
+		<?php echo the_field('icon_two_text');?>
 	</div>
 </div>
 <div class="row promo-icons">
 	<div class="col-xs-12 col-sm-4 col-sm-offset-2">
-		<i class="fa fa-bar-chart"></i>
-		<h3>Car Care & Cleaning</h3>
-		<p>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi nostrum possimus soluta inventore fugiat, modi molestias quod ex ratione, voluptates, magnam ab voluptas! Dignissimos, veniam.
-		</p>
+		<?php echo the_field('icon_three');?>
+		<h3><?php echo the_field('icon_three_heading');?></h3>
+		<?php echo the_field('icon_three_text');?>
 	</div>
 	<div class="col-xs-12 col-sm-4">
-		<i class="fa fa-balance-scale"></i>
-		<h3>Touch-Up Materials</h3>
-		<p>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi nostrum possimus soluta inventore fugiat, modi molestias quod ex ratione, voluptates, magnam ab voluptas! Dignissimos, veniam.
-		</p>
+		<?php echo the_field('icon_four');?>
+		<h3><?php echo the_field('icon_four_heading');?></h3>
+		<?php echo the_field('icon_four_text');?>
 	</div>
 	<div class="col-xs-12">
-		<button type="button" class="btn btn-default promo-button hvr-sweep-to-right">Read more</button>
+		<a href="<?php echo the_field('icons_button_url');?>" class="btn btn-default promo-button hvr-sweep-to-right"><?php echo the_field('icons_button_text');?></a>
 	</div>
 </div>
 
@@ -112,41 +78,23 @@ get_header(); ?>
 	<div class="col-xs-12 col-sm-6">
 	</div>
 	<div class="col-xs-12 col-sm-6 text-center">
-		<h3>Services</h3>
-		<p>
-			ARASYSTEM include the largest range of colors for the Automotive Industry in Poland; leadership in Manufacturing and Distribution Varnishes correction, including the independence thanks to the complete process of production: from dry pigment to the final product.
-		</p>
-		<ul class="list-group">
-			<li class="list-group-item">No Lacquer Required - fully compatible if desired</li>
-			<li class="list-group-item">Computerised Formulation - most accurate match
-				<li class="list-group-item">Durable - Withstands Washing and Adverse Weather Conditions</li>
-				<li class="list-group-item">Fraction of Dealership Prices - exceptional value</li>
-			<li class="list-group-item">Free UK Delivery</li>
-			<li class="list-group-item">UK Company - Same day dispatch available</li>
-		</ul>
-		<button type="button" class="btn btn-default promo-button hvr-sweep-to-right">button</button>
+		<h3><?php echo the_field('offers_heading');?></h3>
+		<?php echo the_field('offers_text');?>
+		<?php echo the_field('offers_list');?>
+		<a href="<?php echo the_field('offers_button_link');?>" class="btn btn-default promo-button hvr-sweep-to-right"><?php echo the_field('offers_button_text');?></a>
 	</div>
 </div>
 
-<div class="row services text-center">
+<div class="row offer text-center">
 		<div class="col-xs-10 col-sm-10 col-sm-offset-1">
 			<div class="col-xs-12 col-sm-4">
-				<img src="http://placehold.it/600x400" class="img-responsive" alt="Image">
-				<p>
-					<a href="#" class="text-uppercase">Engine Diagnostic</a>
-				</p>
+				<a href="<?php echo the_field('promo_one_url');?>" class="text-uppercase"><img src="<?php echo the_field('promo_one_image');?>" class="img-responsive" alt="Image"><?php echo the_field('promo_one_heading');?></a>
 			</div>
 			<div class="col-xs-12 col-sm-4">
-				<img src="http://placehold.it/600x400" class="img-responsive" alt="Image">
-				<p>
-					<a href="#" class="text-uppercase">Engine Diagnostic</a>
-				</p>
+				<a href="<?php echo the_field('promo_two_url');?>" class="text-uppercase"><img src="<?php echo the_field('promo_two_image');?>" class="img-responsive" alt="Image"><?php echo the_field('promo_two_heading');?></a>
 			</div>
 			<div class="col-xs-12 col-sm-4">
-				<img src="http://placehold.it/600x400" class="img-responsive" alt="Image">
-				<p>
-					<a href="#" class="text-uppercase">Engine Diagnostic</a>
-				</p>
+				<a href="<?php echo the_field('promo_three_url');?>" class="text-uppercase"><img src="<?php echo the_field('promo_three_image');?>" class="img-responsive" alt="Image"><?php echo the_field('promo_three_heading');?></a>
 			</div>
 		</div>
 </div>
